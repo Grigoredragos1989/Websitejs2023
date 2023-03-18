19eed8df692c7c7e190350a59fddb22e5d955822
[Grigoredragos1989] <[grigoredragos21@gmail.com]>
2023-03-18 19:59:21 +0200

    18.03

--- shop/dev/js/modal.js
+++ shop/dev/js/modal.js
@@ -3,10 +3,12 @@
 var id;
 $("#modalForm").on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); //Button that triggered the modal
-    const email = button.data('email') //need to find the button and get email
-    const nume = button.data('nume') //need to find the button and get name
-    $(".modal-body #email").val(email);
-    $(".modal-body #name").val(nume);
+    const produs = button.data('name') //need to find the button and get product
+    const pret = button.data('price') //need to find the button and get price
+    const description = button.data('description');
+    $(".modal-header #titleModalLabel").html(produs);
+    $(".modal-body #price").html(pret);
+    $(".modal-body #description").html(description);
     id = button.attr('id');
     // var datainfo = document.getElementById(id);
     alert(JSON.stringify(button[0]));
@@ -20,3 +22,4 @@ $("#modalForm").on('hidden.bs.modal', function (event) {
     document.getElementById(id).classList.remove('btn-primary');
     document.getElementById(id).classList.add('btn-success');
 })
+
